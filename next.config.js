fcbc7755ba63bb83b4757337b5c60c8cd08f35ba
M-Aitisam <aitisamahmed24@gmail.com>
2025-07-13 23:16:44 +0500

    update the Code Create the pOrtfoliop  and resolve conflict packages remove

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   
   // Essential for static export (Cloudflare Pages)
   output: 'export',
